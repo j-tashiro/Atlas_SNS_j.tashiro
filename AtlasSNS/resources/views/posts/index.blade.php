0de6567cdc7e8e0fa19f78b41f9b315f7a65475e
juntaro1126 <blacktoblack345@yahoo.co.jp>
2022-11-16 07:59:06 +0900

    11/16 対応中 ログインユーザーのつぶやきを編集 11/16 対応中 アコーディオンのレイアウト 11/16 対応中 投稿フォームのレイアウト 11/16 対応中 フォロー,フォロワーリストへのリンク先

--- AtlasSNS/resources/views/posts/index.blade.php
+++ AtlasSNS/resources/views/posts/index.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <h2>機能を実装していきましょう。</h2>
 
-<!-- 2022.11.12 投稿フォーム用-->
+<!-- 2022.11.12 投稿フォームの設置-->
         {!! Form::open(['url' => 'posts/create']) !!}
         <div class="form-group">
             {!! Form::input('text', 'newPost', null, ['required', 'class' => 'form-control', 'placeholder' => '投稿内容を入力してください']) !!}
@@ -12,4 +12,15 @@
         <button type="submit"><img src="images/post.png"></button>
         {!! Form::close() !!}
 
+<!-- 2022.11.16 ログインユーザーのつぶやきを編集 -->
+        <div class="">
+            <img src="images/edit.png">
+        </div>
+        {!! Form::open(['url' => '/post/update']) !!}
+        <div class="form-group">
+            {!! Form::hidden('id', $post->id) !!}
+            {!! Form::input('text', 'upPost', $post->post, ['required', 'class' => 'form-control']) !!}
+        </div>
+        <button type="submit" class="btn btn-primary pull-right">更新</button>
+        {!! Form::close() !!}
 @endsection
