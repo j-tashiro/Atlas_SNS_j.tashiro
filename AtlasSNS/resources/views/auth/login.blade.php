2a81a5b3a4f427cf03265620727940c970897e1c
juntaro1126 <blacktoblack345@yahoo.co.jp>
2023-02-24 22:29:06 +0900

    2023/2/23 対応中 ユーザー検索の結果一覧を表示 2023/2/23 対応中 ログイン画面レイアウト
    2023/2/1 対応中 アコーディオンのレイアウト
    2023/2/23 対応中 投稿フォームのレイアウト

--- AtlasSNS/resources/views/auth/login.blade.php
+++ AtlasSNS/resources/views/auth/login.blade.php
@@ -9,12 +9,19 @@
 
 <p>AtlasSNSへようこそ</p>
 
-{{ Form::label('mail adress') }}
-{{ Form::text('mail',null,['class' => 'input']) }}
-{{ Form::label('password') }}
-{{ Form::password('password',['class' => 'input']) }}
+    <div class="form_mail">
+        {{ Form::label('mail adress') }}
+        {{ Form::text('mail',null,['class' => 'input']) }}
+    </div>
 
-{{ Form::submit('LOGIN') }}
+    <div class="form_password">
+        {{ Form::label('password') }}
+        {{ Form::password('password',['class' => 'input']) }}
+    </div>
+
+    <div class="form_submit">
+        {{ Form::submit('LOGIN') }}
+    </div>
 
 <p><a href="/register">新規ユーザーの方はこちら</a></p>
 
