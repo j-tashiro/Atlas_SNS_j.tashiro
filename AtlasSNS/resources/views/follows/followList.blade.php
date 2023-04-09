3e423fe58d0ca1c11813bb3432d0d0118147396f
juntaro1126 <blacktoblack345@yahoo.co.jp>
2023-04-09 21:59:41 +0900

    2023/04/08 対応中 followList.blade.php レイアウト 2023/04/08 対応中 SNS課題 レイアウト 2023/04/08 対応中 search.blade.php レイアウト 2023/04/08 対応中 index.blade.php レイアウト
    2023/04/07 完了 ログインユーザーとフォローしているユーザーのつぶやきのみを表示                                         viewに全部追加してる controllerに追加でできるか確認
    2023/04/07 完了 ログインユーザーのプロフィール編集                                                                                編集してからログインするとログインできないは確認する必要あり
    2023/03/31 完了 ユーザーのアイコン一覧の設置                                                                                     確認必須
    2023/03/30 完了 サイドバー/フォロー,フォロワー数の表示                                                                           論理的に説明できるように講師陣に確認必須

--- AtlasSNS/resources/views/follows/followList.blade.php
+++ AtlasSNS/resources/views/follows/followList.blade.php
@@ -2,28 +2,30 @@
 
 @section('content')
 
-<h2>Follow List</h2>
-
 <!-- http://taustation.com/laravel-uploaded-image-display/ -->
 <div class="main_content follow_content">
-
-<!-- foreachの後は($複数形 as $単数形) が一番綺麗  -->
-<!-- アットマークはコメントアウトしても無駄 超重要 -->
-@foreach ($users as $user)
-    <a href="/user/{{$user->id}}"><img src="{{ \Storage::url($user->image) }}"></a>
-@endforeach
+    <h2>Follow List</h2>
+        <!-- foreachの後は($複数形 as $単数形) が一番綺麗  -->
+        <!-- アットマークはコメントアウトしても無駄 超重要 -->
+        @foreach ($users as $user)
+            <div class="follow_image">
+                <a href="/user/{{$user->id}}"><img src="{{ \Storage::url($user->image) }}"></a>
+            </div>
+        @endforeach
 </div>
 
-<div class="">
+
 @foreach ($posts as $post)
-    <!-- https://www.wakuwakubank.com/posts/377-laravel-relation-1/ -->
-    <!-- $post->user->imageと$post->user->usernameのuserはpost.phpのuserメソッドを指してる -->
-    <img src="{{ \Storage::url($post->user->image) }}">
-    {{ $post->user->username }}
-    {{ $post->post }}
-    {{ $post->created_at }}
+    <div class="follow_list">
+        <!-- https://www.wakuwakubank.com/posts/377-laravel-relation-1/ -->
+        <!-- $post->user->imageと$post->user->usernameのuserはpost.phpのuserメソッドを指してる -->
+        <img src="{{ \Storage::url($post->user->image) }}">
+        {{ $post->user->username }}
+        {{ $post->post }}
+        {{ $post->created_at }}
+    </div>
 @endforeach
-</div>
+
 
 
 
--- AtlasSNS/resources/views/follows/followerList.blade.php
+++ AtlasSNS/resources/views/follows/followerList.blade.php
@@ -2,22 +2,24 @@
 
 @section('content')
 
-<h2>Follower List</h2>
-
-<div class="main_content">
-@foreach ($users as $user)
-    <a href="/user/{{$user->id}}"><img src="{{ \Storage::url($user->image) }}"></a>
-@endforeach
+<div class="main_content follow_content">
+    <h2>Follower List</h2>
+        @foreach ($users as $user)
+            <div class="follow_image">
+                <a href="/user/{{$user->id}}"><img src="{{ \Storage::url($user->image) }}"></a>
+            </div>
+        @endforeach
 </div>
 
-<div class="">
+
 @foreach ($posts as $post)
-    <img src="{{ \Storage::url($post->user->image) }}">
-    {{ $post->user->username }}
-
-    {{ $post->post }}
-    {{ $post->created_at }}
+    <div class="follower_list">
+        <img src="{{ \Storage::url($post->user->image) }}">
+        {{ $post->user->username }}
+        {{ $post->post }}
+        {{ $post->created_at }}
+    </div>
 @endforeach
-</div>
+
 
 @endsection
