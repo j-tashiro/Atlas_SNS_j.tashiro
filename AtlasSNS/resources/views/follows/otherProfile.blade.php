4f4598a290112665ce5b7f8d4feb8ee026824d31
juntaro1126 <blacktoblack345@yahoo.co.jp>
2023-04-13 21:14:42 +0900

    2023/04/12 対応中 index.blade.php レイアウト 2023/04/12 対応中 userProfile.blade.php レイアウト 2023/04/12 対応中 otherProfile.blade.php レイアウト 2023/04/12 対応中 followList.blade.php レイアウト 2023/04/12 対応中 followerList.blade.php レイアウト 2023/04/12 対応中 SNS課題 レイアウト
    2023/04/07 完了 ログインユーザーとフォローしているユーザーのつぶやきのみを表示                                         viewに全部追加してる controllerに追加でできるか確認
    2023/04/07 完了 ログインユーザーのプロフィール編集                                                                                編集してからログインするとログインできないは確認する必要あり アイコンとbioが空白だと更新できない

--- AtlasSNS/resources/views/follows/otherProfile.blade.php
+++ AtlasSNS/resources/views/follows/otherProfile.blade.php
@@ -7,21 +7,27 @@
 <div class="main_content">
     <div class="other_profile">
         <img src="{{ \Storage::url($user->image) }}">
+
             <div class="username_bio">
+
                 <div class="other_username">
                     <p>name</p>
                     <p>{{ $user->username }}</p>
                 </div>
+
                 <div class="other_bio">
                     <p>bio</p>
                     <p>{{ $user->bio }}</p>
+
+                    </div>
+            </div>
+            <div class="follow_btn">
                     @if (auth()->user()->isFollowing($user->id))
                         <p class="btn red"><a href="/users/{{ $user->id }}/unfollow">フォローを解除</a></p>
                     @else
                         <p class="btn light_blue"><a href="/users/{{ $user->id }}/follow">フォローする</a></p>
                     @endif
-                </div>
-            </div>
+                    </div>
     </div>
 </div>
 
